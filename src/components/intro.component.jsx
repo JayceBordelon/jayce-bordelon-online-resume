6b87a0661afef73745e0bbb9ea8926729cf74fae
JayceBordelon <b.jayce@wustl.edu>
2023-07-22 01:36:52 -0700

    Final Bug Fixes

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -15,7 +15,7 @@ export default class Intro extends Component {
     return (
       <Container textAlign='center' className="intro-wrapper">
       <Image src={Picture} onClick={this.props.scrollToBottom} className="intro-profile-pic"/>
-        <span><h1 className="intro-typer-text"><Typer/> </h1></span>
+        <span className="intro-typer-text"><h1 className="intro-typer-text"><Typer/> </h1></span>
       </Container>
       
     );

97f900a9e2d2088f26c7159875ffba57f5afec6a
JayceBordelon <b.jayce@wustl.edu>
2023-07-30 22:48:55 -0700

    Skills update

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -1,6 +1,6 @@
 //Dependencies
 import React, { Component } from 'react';
-import { Container, Modal, Button, Icon} from 'semantic-ui-react';
+import { Container} from 'semantic-ui-react';
 
 //Components (Icons)
 import { DiCss3, DiNodejs, DiPython, DiReact, DiRuby, DiHtml5, DiJava, DiJavascript1  } from 'react-icons/di' // from devicons
@@ -8,7 +8,6 @@ import {TbBrandCpp, TbApi, TbBrandDjango} from 'react-icons/tb'
 import {SiAssemblyscript, SiAwslambda, SiKubernetes, SiDocker, SiMongodb, SiPostgresql, SiSvelte} from 'react-icons/si'
 import {FaLinux, FaGit} from 'react-icons/fa'
 import {GiArtificialHive} from 'react-icons/gi'
-import Null from './null.component'
 
 
 //Style
@@ -17,11 +16,6 @@ import '../styles/wrapper.css'
 
 export default class Skills extends Component {
 
-
-  state = {
-    'showModal': false,
-    'currentExp': {}
-  }
 
   titles = {
     ass: 'Teachers Assistant @ WashU',
@@ -37,42 +31,50 @@ export default class Skills extends Component {
 
 
     languages = [
-      {name: 'CSS', comp: <DiCss3 size={100}/>, rel: this.titles.mag, yrs: '~3 years'},
-      {name: 'Node.js', comp: <DiNodejs size={100}/>, rel: this.titles.mag, yrs: '~1 year'},
-      {name: 'Python', comp: <DiPython size={100}/>, rel: this.titles.iti, yrs: '~3 years'},
-      {name: 'React.js', comp: <DiReact size={100}/>, rel: this.titles.iti, yrs: '~1 year'},
-      {name: 'HTML', comp: <DiHtml5 size={100}/>, rel: this.titles.tut, yrs: '~4 years'},
-      {name: 'Java', comp: <DiJava size={100}/>, rel: this.titles.ass, yrs: '~4 years'},
-      {name: 'JavaScript', comp: <DiJavascript1 size={100}/>, rel: this.titles.ful, yrs: '~4 years'},
-      {name: 'C++', comp: <TbBrandCpp size={100}/>, rel: this.titles.edu, yrs: '~1 year'},
-      {name: 'AVR Assembly', comp: <SiAssemblyscript size={90}/>, rel: this.titles.edu, yrs: '~1 year'},
-      {name: 'Kubernetes (conceptual)', comp: <SiKubernetes size={100}/>, rel: this.titles.iti, yrs: '~1 year'},
-      {name: 'Docker (conceptual)', comp: <SiDocker size={100}/>, rel: this.titles.iti, yrs: '~1 year'},
-      {name: 'Svelte (conceptual)', comp: <SiSvelte size={100}/>, rel: this.titles.own, yrs: '~1 year'},
+      {name: 'CSS', comp: <DiCss3 size={100}/>, rel: this.titles.mag, yrs: '~3 years', active: false},
+      {name: 'Node.js', comp: <DiNodejs size={100}/>, rel: this.titles.mag, yrs: '~1 year', active: false},
+      {name: 'Python', comp: <DiPython size={100}/>, rel: this.titles.iti, yrs: '~3 years', active: false},
+      {name: 'React.js', comp: <DiReact size={100}/>, rel: this.titles.iti, yrs: '~1 year', active: false},
+      {name: 'HTML', comp: <DiHtml5 size={100}/>, rel: this.titles.tut, yrs: '~4 years', active: false},
+      {name: 'Java', comp: <DiJava size={100}/>, rel: this.titles.ass, yrs: '~4 years', active: false},
+      {name: 'JavaScript', comp: <DiJavascript1 size={100}/>, rel: this.titles.ful, yrs: '~4 years', active: false},
+      {name: 'C++', comp: <TbBrandCpp size={100}/>, rel: this.titles.edu, yrs: '~1 year', active: false},
+      {name: 'AVR Assembly', comp: <SiAssemblyscript size={90}/>, rel: this.titles.edu, yrs: '~1 year', active: false},
+      {name: 'Kubernetes (conceptual)', comp: <SiKubernetes size={100}/>, rel: this.titles.iti, yrs: '~1 year', active: false},
+      {name: 'Docker (conceptual)', comp: <SiDocker size={100}/>, rel: this.titles.iti, yrs: '~1 year', active: false},
+      {name: 'Svelte (conceptual)', comp: <SiSvelte size={100}/>, rel: this.titles.own, yrs: '~1 year', active: false},
     ]
     web_frameworks = [
-      {name: 'Ruby on Rails', comp: <DiRuby size={95}/>, rel: this.titles.ful, yrs: '~3 years'},
-      {name: 'MERN stack', comp: <SiMongodb size={100}/>, rel: this.titles.mag, yrs: '~1 year'},
-      {name: 'Python Django', comp: <TbBrandDjango size={100}/>, rel: this.titles.iti, yrs: '~1 year'},
+      {name: 'Ruby on Rails', comp: <DiRuby size={95}/>, rel: this.titles.ful, yrs: '~3 years', active: false},
+      {name: 'MERN stack', comp: <SiMongodb size={100}/>, rel: this.titles.mag, yrs: '~1 year', active: false},
+      {name: 'Python Django', comp: <TbBrandDjango size={100}/>, rel: this.titles.iti, yrs: '~1 year', active: false},
     ]
     db_and_other = [
-      {name: 'Git', comp: <FaGit size={100}/>, rel: this.titles.iti, yrs: '~2 years'},
-      {name: 'APi Development', comp: <TbApi size={100}/>, rel: this.titles.mag, yrs: '~2 years'},
-      {name: 'OCR Technology', comp: <GiArtificialHive size={100}/>, rel: this.titles.bac, yrs: '~2 years'},
-      {name: 'Linux (WSL)', comp: <FaLinux size={100}/>, rel: this.titles.iti, yrs: '~1 years'},
-      {name: 'PostgreSQL (PSQL)', comp: <SiPostgresql size={100}/>, rel: this.titles.dat, yrs: '~3 years'},
-      {name: 'AWS Lambda', comp: <SiAwslambda size={100}/>, rel: this.titles.own, yrs: '~2 months'},
+      {name: 'Git', comp: <FaGit size={100}/>, rel: this.titles.iti, yrs: '~2 years', active: false},
+      {name: 'APi Development', comp: <TbApi size={100}/>, rel: this.titles.mag, yrs: '~2 years', active: false},
+      {name: 'OCR Technology', comp: <GiArtificialHive size={100}/>, rel: this.titles.bac, yrs: '~2 years', active: false},
+      {name: 'Linux (WSL)', comp: <FaLinux size={100}/>, rel: this.titles.iti, yrs: '~1 years', active: false},
+      {name: 'PostgreSQL (PSQL)', comp: <SiPostgresql size={100}/>, rel: this.titles.dat, yrs: '~3 years', active: false},
+      {name: 'AWS Lambda', comp: <SiAwslambda size={100}/>, rel: this.titles.own, yrs: '~2 months', active: false},
     ]
 
-    toComponent = (lang) => {
+    state = {
+      activeLanguage: this.languages.find((language) => language.name === 'JavaScript')
+    }
+  
+
+    getIcon = (lang) => {
       return lang.comp;
     }
 
-    handleModal = (exp) => {
-      this.setState({showModal: !this.state.showModal})
-      this.setState({currentExp: exp})
-      console.log(exp.long)
+    setActive = (lang) => {
+      if (!lang.active){
+        lang.active = true;
+        this.forceUpdate();
+      }
     }
+
+
 
     render() {
       this.languages.sort((a, b) => a.name.localeCompare(b.name));
@@ -80,48 +82,78 @@ export default class Skills extends Component {
       this.db_and_other.sort((a, b) => a.name.localeCompare(b.name));
       return (
         <>
-          {this.state.showModal ? (
-            <Modal className="work-modal" open={true}>
-              <span className="work-modal-icon">
-                {this.toComponent(this.state.currentExp)}
-                <h1>{this.state.currentExp.name}</h1>
-              </span>
-              <h1>Most Relevant Experience: {this.state.currentExp.rel}</h1>
-              <h4>I have been working with this technology for {this.state.currentExp.yrs}</h4>
-              <Button onClick={() => this.setState({ showModal: false })} className="cool-button">
-                <p>
-                  <Icon name='checkmark' size="large" /> Close
-                </p>
-              </Button>
-            </Modal>
-          ) : (
-            <Null />
-          )}
-    
-          <Container textAlign='center' className="skills-wrapper">
+          <Container className="skills-wrapper">
             <h1>Languages</h1>
+            <div className="skills-grid">
             {this.languages.map(lang => (
-              <span className="skills-icon" onClick={() => this.handleModal(lang)}>
-                {this.toComponent(lang)}
+              lang.active ? (
+                <span className="active-skill"
+                >
+                {this.getIcon(lang)}
+                <p>Most relavent experience - {lang.rel}</p>
+                <p>{lang.yrs} of experience.</p>
+                
+              </span>
+              ) : 
+              
+              (
+              <span className="skills-icon" 
+                onMouseEnter={() => this.setActive(lang)}
+              >
+                {this.getIcon(lang)}
                 <h4>{lang.name}</h4>
               </span>
+              )
             ))}
+            </div>
     
             <h1>Web Frameworks</h1>
+            <div className="skills-grid">
             {this.web_frameworks.map(lang => (
-              <span className="skills-icon" onClick={() => this.handleModal(lang)}>
-                {this.toComponent(lang)}
+              lang.active ? (
+                <span className="active-skill"
+                >
+                {this.getIcon(lang)}
+                <p>Most relavent experience - {lang.rel}</p>
+                <p>{lang.yrs} of experience.</p>
+                
+              </span>
+              ) : 
+              
+              (
+              <span className="skills-icon" 
+                onMouseEnter={() => this.setActive(lang)}
+              >
+                {this.getIcon(lang)}
                 <h4>{lang.name}</h4>
               </span>
+              )
             ))}
+            </div>
     
             <h1>Databases & Other</h1>
+            <div className="skills-grid">
             {this.db_and_other.map(lang => (
-              <span className="skills-icon" onClick={() => this.handleModal(lang)}>
-                {this.toComponent(lang)}
+              lang.active ? (
+                <span className="active-skill"
+                >
+                {this.getIcon(lang)}
+                <p>Most relavent experience - {lang.rel}</p>
+                <p>{lang.yrs} of experience.</p>
+                
+              </span>
+              ) : 
+              
+              (
+              <span className="skills-icon" 
+                onMouseEnter={() => this.setActive(lang)}
+              >
+                {this.getIcon(lang)}
                 <h4>{lang.name}</h4>
               </span>
+              )
             ))}
+            </div>
           </Container>
         </>
       )

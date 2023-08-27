63da536b551aadae2b6b92ed354a3b73cbef68da
JayceBordelon <b.jayce@wustl.edu>
2023-08-27 12:53:32 -0500

    Domain info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 //Dependencies
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import {Helmet} from 'react-helmet'
 //Components
 import Wrapper from './app_wrapper/wrapper.component';
 //styles
@@ -10,13 +9,6 @@ import "./styles/wrapper.css"
 function App() {
   return (
     <Router>
-      <Helmet>
-        <title>Jayce Bordelon - Online Resume</title>
-      </Helmet>
-      <meta
-          name="description"
-          content= "Hello! I would like to welcome you to my site! I am a passionate Computer Science and Finance student with expertise in Human-Computer Interaction. Get to know me here and reach out if my skills are of value or interest! Thank you for your time! :)"
-        />
           <Routes>
             <Route exact path="/" element={<Wrapper />} />
             <Route path="/education" element={<Wrapper/>} /> 

490a99cb1d5fe0a379404f23e58b4d869b13cd7e
JayceBordelon <b.jayce@wustl.edu>
2023-10-23 23:08:02 -0500

    font and label

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -17,7 +17,7 @@ export default class Intro extends Component {
       <>
       <Container id="intro" className="intro-wrapper super-fade" textAlign='center' onClick={() => {window.open("https://www.linkedin.com/in/jaycebordelon", "_blank")}}>
       <Image src={Picture} onClick={this.props.scrollToBottom} className="intro-profile-pic me-in fancy-boxen"/>
-      <h1><Typer text={`I Build Valuable Software.`} className="strongest"/></h1>
+      <h1><Typer text={`Learning To Build Valuable Software.`} className="strongest"/></h1>
       </Container>
       </>
       

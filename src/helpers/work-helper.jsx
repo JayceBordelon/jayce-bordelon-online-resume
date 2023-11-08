23bafca419aa6a2eda6d29b680aecb1a858620ad
Jayce Bordelon <jaycebordelon@Jayces-MBP.lan>
2023-11-07 23:57:14 -0600

    mastercard

--- src/helpers/work-helper.jsx
+++ src/helpers/work-helper.jsx
@@ -25,7 +25,7 @@ import MagnifiLogo from "../styles/mgfi-logo.webp"
   export function WorkHelper() {
     const [experiences, setExperiences] = useState([
       {when: 'Incoming Spring 2024', 
-      title: 'Software Engineering Intern', 
+      title: 'Software Engineering Co-Op Intern', 
       long: "Software Engineering Intern Spring 2024 Co-Op",
       accomplishments: [
         

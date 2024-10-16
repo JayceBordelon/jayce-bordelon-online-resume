08ac4a606589ab2320a396e93c2c3880500c28b3
Jayce Bordelon <jaycebordelon@Jayces-MacBook-Pro.local>
2024-10-15 19:59:59 -0500

    Removing some projects to make new ones

--- src/helpers/projects-helper.jsx
+++ src/helpers/projects-helper.jsx
@@ -64,7 +64,7 @@ export const hackWashu = {
       }
       
     }
-    const allProjects = [jayceBlogs, flome, bearsAndBulls, blackJack, magnifi, hackWashu]
+    const allProjects = [jayceBlogs, flome, bearsAndBulls]
     return (
       <>
       {generateHeader("Some of my work")}
